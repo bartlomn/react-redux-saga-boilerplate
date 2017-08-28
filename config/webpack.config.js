a08bf2fe2ffb056a5cde893e84f6a1a9f3e4851f
BART NOWAK <bnowak@bnowak.com>
2017-08-28 11:06:42 +0200

    + better webpack config management

---
+++ config/webpack.config.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+
+
+const PATHS = {
+  app: path.join(__dirname, '..', 'app'),
+  build: path.join(__dirname, '..', 'build'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new FriendlyErrorsWebpackPlugin(),
+      new DashboardPlugin({ port: process.env.PORT }),
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavaScript({ include: PATHS.app, options: { emitWarning: true } }),
+]);
+
+const productionConfig = merge([
+]);
+
+const developmentConfig = merge([
+  parts.devServer({ host: process.env.HOST, port: process.env.PORT }),
+]);
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
+};
