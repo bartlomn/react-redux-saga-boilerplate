2121f6b8636d6ad0b8c409a0a712bb462eba696d
BART NOWAK <bnowak@bnowak.com>
2017-09-27 13:25:40 +0200

    + code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,13 @@
 const karmaMocha = require( 'karma-mocha' );
 const pjsLauncher = require( 'karma-phantomjs-launcher' );
 const karmaWebpack = require( 'karma-webpack' );
+const karmaCover = require( 'karma-coverage' );
 
 const webpack = require( './config/webpack.mocha' );
 
 module.exports = ( config ) => {
+  process.env.BABEL_ENV = 'karma';
+
   const tests = 'app/**/*.test.js';
   const src = 'app/**/*.js';
 
@@ -24,6 +27,14 @@ module.exports = ( config ) => {
     webpackMiddleware: {
       noInfo: false,
     },
-    plugins: [ karmaMocha, pjsLauncher, karmaWebpack ],
+    plugins: [ karmaMocha, pjsLauncher, karmaCover, karmaWebpack ],
+    reporters: [ 'coverage' ],
+    coverageReporter: {
+      dir: 'build',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcov' },
+      ],
+    },
   });
 };
