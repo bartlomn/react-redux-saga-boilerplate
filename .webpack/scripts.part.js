829faa043f404f5976138290c6e7d2fcd09620f8
BART NOWAK <bnowak@bnowak.com>
2017-10-03 20:13:39 +0200

    🔧 disabled webpack cache

--- .webpack/scripts.part.js
+++ .webpack/scripts.part.js
@@ -13,7 +13,8 @@ exports.transpileJavaScript = ({ include, exclude }) => ({
           // It uses default OS directory by default. If you need
           // something more custom, pass a path to it.
           // I.e., { cacheDirectory: '<path>' }
-          cacheDirectory: './.webpack/cache/',
+          // cacheDirectory: './.webpack/cache/',
+          cacheDirectory: false,
         },
       },
     ],
